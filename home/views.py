c383729b216d3f47c19a16d6048958736f15952f
Peter Dugery <pdugery@gmail.com>
2022-10-17 19:53:10 -0400

    Created signin and signup pages

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+from .models import Announcements
+
 def home(request):
-    return render(request, 'home/home.html', {})
+    all_announcements = Announcements.objects.all()
+    return render(request, 'home/home.html', {'announcements': all_announcements})
 
 def competition(request):
     return render(request, 'home/competition.html', {})
 
 def team(request):
     return render(request, 'home/team.html', {})
+
+def signin(request):
+    return render(request, 'home/signin.html', {})
+
+def signup(request):
+    return render(request, 'home/signup.html', {})
